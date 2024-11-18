b92c0b1029c9d07bb8c432c925134b9b3cca8f7d
Mikkel Andersen <miande22@student.aau.dk>
2024-11-18 13:50:46 +0100

    Cleaned up the database. Removed database variable, now hash codes are stored in a dictionary with key being the image id, and value being the hashcode

--- metrics/hash_lookup.py
+++ metrics/hash_lookup.py
@@ -1,65 +1,38 @@
-import pickle
-import sys
 from typing import Callable
 
 import numpy as np
+import torch
 
 
 class Database:
     def __init__(self):
-        # List of all images in buckets, the hashcode for a given bucket is the same index in the hashcode table
-        self.database = []
-        # List of hash-codes
-        self.hashcodes = []
+        # List of hash-codes (id of image, hashcode)
+        self.hash_codes = {}
 
 
     def __getitem__(self, key) -> list | None:
-        index = self.get_hash_code_index(key)
-        return self.database[index] if index is not None else None
+        return self.hash_codes[key]
 
 
-    def get_image_hash_code(self, id: int):
-        for i in range(len(self.database)):
-            for j in range(len(self.database[i])):
-                if self.database[i][j] == id:
-                    return self.hashcodes[i]
-        return None
+    def __len__(self):
+        return len(self.hash_codes)
+
 
     def get_hash_code_index(self, key) -> int | None:
-        for i, hashcode in enumerate(self.hashcodes):
+        for i, hashcode in enumerate(self.hash_codes.values()):
             if np.array_equal(hashcode, key):
                 return i
         return None
 
-    def get_images_with_distance_sorted(self, query, max_distance):
-        dist_to_indexes = [None] * (max_distance + 1)
+    def hash_codes_as_np_array(self):
+        return np.array([code for code in self.hash_codes.values()])
 
-        for i, code in enumerate(self.hashcodes):
-            distance = self.hamming_distance(query, code)
-            if distance <= max_distance:
-                if distance in dist_to_indexes:
-                    dist_to_indexes[distance].append(i)
-                else:
-                    dist_to_indexes[distance] = [i]
-
-        sorted = []
-        for xs in dist_to_indexes:
-            if xs == None:
-                continue
-            sorted += xs
-
-        return sorted
+    def hash_codes_as_tensor(self):
+        return torch.tensor([code for code in self.hash_codes.values()])
 
 
     def __setitem__(self, key, value) -> None:
-        index = self.get_hash_code_index(key)
-        if index is None:
-            # Converts all -1 in hashcode to 0
-            key[key == -1] = 0
-            self.database.append([value])
-            self.hashcodes.append(key)
-        else:
-            self.database[index].append(value)
+        self.hash_codes[value] = np.array(key).flatten().tolist()
 
     """
     Finds all neighbors in the Database within a given distance
@@ -71,28 +44,10 @@ class Database:
     """
     def get_neighbors(self, query, max_distance: int = 5) -> dict:
         neighbors = {}
-        for i, code in enumerate(self.hashcodes):
-            distance = self.hamming_distance(query, code)
+        for i, item in enumerate(self.hash_codes.items()):
+            distance = self.hamming_distance(query, item[1])
             if distance <= max_distance:
-                neighbors[i] = distance
-        return neighbors
-
-
-    def get_nearest_neighbors(self, query, amount: int = 10) -> list:
-        neighbors = []
-        # Get all neighbors
-        neighbors_distance = self.get_neighbors(query, max_distance=sys.maxsize)
-
-        # Sorts by distance first then index in the array
-        distance_sorted = dict(sorted(neighbors_distance.items(), key=lambda item: (item[1], item[0])))
-
-        for key in distance_sorted.keys():
-            images = self.database[key]
-            for image in images:
-                if len(neighbors) < amount:
-                    neighbors.append(image)
-                else:
-                    return neighbors
+                neighbors[item[0]] = distance
         return neighbors
 
     """
@@ -128,16 +83,3 @@ def pre_gen_hash_codes(model_query: Callable, dataset: list[tuple[int, list, int
     print()
 
     return db
-
-# Test function please delete
-def generate_binary_hash(input_data, hash_size=4):
-    hash_value = hash(input_data.tobytes())
-    binary_hash = bin(hash_value & int('1' * hash_size, 2))[2:].zfill(hash_size)
-    return np.array([int(bit) for bit in binary_hash], dtype=int)
-
-if __name__ == "__main__":
-    with open("cifar-10-batches-py/data_batch_1", 'rb') as fo:
-        data = pickle.load(fo, encoding='bytes')[b'data'][:100]
-    db = pre_gen_hash_codes(generate_binary_hash, data)
-    neighbors = db.get_nearest_neighbors(generate_binary_hash(data[0]))
-    print("Images ids of the", len(neighbors), "nearest neighbors:", neighbors)
