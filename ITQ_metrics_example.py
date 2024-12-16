24d0231dac81d7abfb8a25a965fb49e153e304d9
Mikkel Andersen <miande22@student.aau.dk>
2024-12-16 11:12:04 +0100

    Added calculate encoding times

--- ITQ_metrics_example.py
+++ ITQ_metrics_example.py
@@ -1,6 +1,13 @@
 from models.ITQ.hashing.itq_model import *
 from utility.feature_loader import FeatureLoader
-from utility.metrics_framework import MetricsFramework
+from utility.metrics_framework import calculate_encoding_time, save_results, MetricsFramework
+
+# The traditional methods use pre extracted features.
+# The encode times are an average over all datasets (Encoding times was within 0.2ms) using 1000 single queries for each.
+# Preprocessing times varied between datasets, so they are in an array [mnist, cifar, nuswide]
+gpu_encode = 1.244
+cpu_encode = 14.130
+preprocessing = [1.102, 1.068, 1.801]
 
 ################################################################
 ############################ Config ############################
@@ -10,13 +17,20 @@ query_size = 10000
 encode_len = [8, 16, 32, 64]
 dataset_names = ["mnist", "cifar-10", "nuswide"]
 
-for dataset_name in dataset_names:
+for i, dataset_name in enumerate(dataset_names):
     fl = FeatureLoader(dataset_name)
 
-    for i in range(len(encode_len)):
-        print("---------------- Calculating metrics for bit length", encode_len[i], "----------------")
-        model = ITQ(encode_len[i])
+    encoding_times = {}
+
+    for encode_length in encode_len:
+        print("---------------- Calculating metrics for bit length", encode_length, "----------------")
+        model = ITQ(encode_length)
         model.fit(fl.data[query_size:])
 
         metrics_framework = MetricsFramework(model.encode, fl, query_size, multi_encoder=True)
-        metrics_framework.calculate_metrics(dataset_name + "/ITQ_" + str(encode_len[i]) + "_bits_" + dataset_name, k)
+        metrics_framework.calculate_metrics(dataset_name + "/ITQ_" + str(encode_length) + "_bits_" + dataset_name, k)
+        gpu_mean, cpu_mean, _ = calculate_encoding_time(encode_length, fl, query_cpu=model.encode_single)
+        bit_times = {"gpu": gpu_mean, "cpu": cpu_mean, "processing": 0, "total_gpu": cpu_mean + gpu_encode + preprocessing[i],
+                     "total_cpu": cpu_mean + cpu_encode + preprocessing[i]}
+        encoding_times[encode_length] = bit_times
+    save_results(encoding_times, dataset_name + "/ITQ_encoding_times")
--- lsh_metrics_example.py
+++ lsh_metrics_example.py
@@ -2,7 +2,14 @@ import numpy as np
 
 from models.lsh import Lsh
 from utility.feature_loader import FeatureLoader
-from utility.metrics_framework import MetricsFramework
+from utility.metrics_framework import calculate_encoding_time, save_results, MetricsFramework
+
+# The traditional methods use pre extracted features.
+# The encode times are an average over all datasets (Encoding times was within 50ms) using 1000 single queries for each.
+# Preprocessing times varied between datasets, so they are in an array [mnist, cifar, nuswide]
+gpu_encode = 1.244
+cpu_encode = 14.130
+preprocessing = [1.102, 1.068, 1.801]
 
 ################################################################
 ############################ Config ############################
@@ -12,14 +19,16 @@ query_size = 10000
 encode_len = [8, 16, 32, 64]
 dataset_names = ["mnist", "cifar-10", "nuswide"]
 
-for dataset_name in dataset_names:
+for i, dataset_name in enumerate(dataset_names):
     fl = FeatureLoader(dataset_name)
     features = np.asarray(fl.data[query_size:])
 
-    for i in range(len(encode_len)):
+    encoding_times = {}
+
+    for encode_length in encode_len:
         # Set LSH parameters
         num_tables = 1
-        num_bits_per_table = encode_len[i]
+        num_bits_per_table = encode_length
         pca_components = 50
 
         # Initialize LSH with the features data
@@ -31,4 +40,9 @@ for dataset_name in dataset_names:
         )
 
         metrics_framework = MetricsFramework(image_lsh.query, fl, query_size)
-        metrics_framework.calculate_metrics(dataset_name + "/LSH_" + str(encode_len[i]) + "_bits_" + dataset_name, k)
+        metrics_framework.calculate_metrics(dataset_name + "/LSH_" + str(encode_length) + "_bits_" + dataset_name, k)
+        gpu_mean, cpu_mean, _ = calculate_encoding_time(encode_length, fl, query_cpu=image_lsh.query)
+        bit_times = {"gpu": gpu_mean, "cpu": cpu_mean, "processing": 0, "total_gpu": cpu_mean + gpu_encode + preprocessing[i],
+                     "total_cpu": cpu_mean + cpu_encode + preprocessing[i]}
+        encoding_times[encode_length] = bit_times
+    save_results(encoding_times, dataset_name + "/LSH_encoding_times")
